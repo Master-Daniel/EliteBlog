f527493fd98b6b09f8cdb8d11ea613d2b52a7986
Master-Daniel <engrdanywiss@gmail.com>
2025-03-14 17:40:22 +0100

    added new routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,18 @@
 import { createBrowserRouter } from "react-router-dom";
+
 import Welcome from "../pages/Welcome";
 import Authors from "../pages/Authors";
 import Author from "../pages/AuthorPage";
 import Contact from "../pages/Contact";
-
+import Layout from "../pages/dashboard/Layout";
+import Dashboard from "../pages/dashboard/Welcome";
+import CreatePost from "../pages/dashboard/CreatePost";
+import CategoryPage from "../pages/dashboard/Category";
+import PostListPage from "../pages/dashboard/PostList";
+import CommentManagementPage from "../pages/dashboard/CommentManagement";
+import GeneralSettingsPage from "../pages/dashboard/GeneralSettings";
+import AdminLayout from "../pages/dashboard/AdminLayout";
+import AdminPostApprovalPage from "../pages/dashboard/AdminPostApproval";
 
 const routes = createBrowserRouter([
     {
@@ -13,7 +22,26 @@ const routes = createBrowserRouter([
             { path: 'authors', element: <Authors /> },
             { path: 'author/:name', element: <Author /> },
             { path: 'contact', element: <Contact /> },
-            { path: '/auth/:provider', element: <Welcome /> },
+            { path: '/auth/github/callback', element: <Welcome /> },
+            {
+                path: 'dashboard',
+                element: <Layout />,
+                children: [
+                    { index: true, element: <Dashboard />},
+                    { path: 'create-post', element: <CreatePost />},
+                    { path: 'post-list', element: <PostListPage />},
+                    { path: 'comment-management', element: <CommentManagementPage />},
+                    { path: 'categories', element: <CategoryPage />},
+                    { 
+                        path: 'admin', 
+                        element: <AdminLayout />,
+                        children: [
+                            { index: true, element: <GeneralSettingsPage /> },
+                            { path: 'verify-post', element: <AdminPostApprovalPage /> },
+                        ]
+                    },
+                ]
+            }
             // { path: "*", element: <NotFound /> }, // Catch-all for unknown routes
         ],
     },
