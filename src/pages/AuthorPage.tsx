3de018ed00a8e895155a7013fbb2b5e62486f9f7
Master-Daniel <engrdanywiss@gmail.com>
2025-03-31 12:19:48 +0100

    updated darK/light mode

--- src/pages/AuthorPage.tsx
+++ src/pages/AuthorPage.tsx
@@ -25,7 +25,7 @@ const Author: React.FC = () => {
     return (
         <>
             <Header />
-            <div className="max-w-[1480px] mx-auto px-5 sm:px-8 mt-20">
+            <div className="max-w-[1480px] mx-auto px-5 sm:px-8 mt-20 text-black dark:text-white">
                 <div className="max-w-[450px] text-center mx-auto mb-20">
                     <div className="rounded-full overflow-hidden border-4 border-gray-200 dark:border-gray-700 inline-block leading-[0] author-avatar">
                         <div className="pt-[100%] relative">
