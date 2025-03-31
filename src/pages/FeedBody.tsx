91117495a4f8a1b2ba195890a1a4b64146d23dce
Master-Daniel <engrdanywiss@gmail.com>
2025-03-31 12:24:37 +0100

    fix: - added diqus for comment - updated the structure of the meta component (added optional props)

--- src/pages/FeedBody.tsx
+++ src/pages/FeedBody.tsx
@@ -9,6 +9,7 @@ import { formatDate } from "../utils/custom-functions";
 import { useSelector } from "react-redux";
 import { RootState } from "../redux/store";
 import { Feed } from "../utils/types";
+import DisqusComments from "../components/DisqusComments ";
 
 const FeedBody: React.FC = () => {
     const params = useParams();
@@ -28,10 +29,22 @@ const FeedBody: React.FC = () => {
 
     return (
         <>
-            {data && <Meta meta={data} />}
+            {data && (
+                <Meta
+                    meta={{
+                        ...data,
+                        noIndex: false,
+                        author: data.author ?? undefined, // Convert null to undefined
+                        schema: data.schema
+                            ? { mainEntityOfPage: { "@id": data.schema.mainEntityOfPage["@id"] } }
+                            : undefined, // Ensure correct type
+                    }}
+                />
+            )}
+
             <Header />
 
-            <div className="max-w-[1480px] mx-auto px-5 sm:px-8 mt-20">
+            <div className="max-w-[1480px] mx-auto px-5 sm:px-8 mt-20 z-10 text-black dark:text-white">
                 <div className="max-w-screen-md mx-auto">
                     <div className="flex flex-wrap gap-3 items-center text-[15px]">
                         <div className="flex flex-wrap gap-3">
@@ -106,25 +119,32 @@ const FeedBody: React.FC = () => {
                         ))}
                     </div>
 
-                    <div className="flex items-center mt-16"><span className="font-semibold mr-4 text-sm uppercase data-color">Share:</span>
+                    <div className="flex items-center mt-16">
+                        <span className="font-semibold mr-4 text-sm uppercase data-color">Share:</span>
                         <div className="flex gap-4">
                             <Link className="text-md border-2 p-2.5 rounded-full text-black border-black hover:bg-black hover:text-white dark:text-white dark:border-white dark:hover:bg-white dark:hover:text-black" to={`https://x.com/intent/tweet?url=${data?.schema?.mainEntityOfPage["@id"]}`} target="_blank" rel="noreferrer" aria-label="Twitter share button">
-                                <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                                <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"></path>
                                 </svg>
                             </Link>
                             <Link className="text-md border-2 p-2.5 rounded-full text-black border-black hover:bg-black hover:text-white dark:text-white dark:border-white dark:hover:bg-white dark:hover:text-black" to={`https://www.facebook.com/sharer.php?u=${data?.schema?.mainEntityOfPage["@id"]}`} target="_blank" rel="noreferrer" aria-label="Facebook share button">
-                                <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 320 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                                <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 320 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z"></path>
                                 </svg>
                             </Link>
                             <Link className="text-md border-2 p-2.5 rounded-full text-black border-black hover:bg-black hover:text-white dark:text-white dark:border-white dark:hover:bg-white dark:hover:text-black" to={`https://www.linkedin.com/sharing/share-offsite/?url=${data?.schema?.mainEntityOfPage["@id"]}`} target="_blank" rel="noreferrer" aria-label="LinkedIn share button">
-                                <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 448 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                                <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 448 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"></path>
                                 </svg>
                             </Link>
                         </div>
                     </div>
+
+                    <DisqusComments
+                        title={data?.title ?? "Default Title"}
+                        description={data?.description ?? "Default Description"}
+                        slug={data?.slug ?? "default-slug"}
+                    />
 
                 </div>
 
