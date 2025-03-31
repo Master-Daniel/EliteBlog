c430a3a2584b8f12b6b07f75abc0a340323ad914
Master-Daniel <engrdanywiss@gmail.com>
2025-03-31 12:20:39 +0100

    updated light/dark mode

--- src/pages/Authors.tsx
+++ src/pages/Authors.tsx
@@ -13,7 +13,7 @@ const Authors: React.FC = () => {
     return (
         <>
             <Header />
-            <div className="max-w-[1480px] mx-auto px-5 sm:px-8 mt-20">
+            <div className="max-w-[1480px] mx-auto px-5 sm:px-8 mt-20 text-black dark:text-white">
                 <div className="mb-24 text-center max-w-screen-sm mx-auto">
                     <h2 className="text-3xl sm:text-5xl capitalize">Get to Know Our Authors</h2>
                     <p className="mt-4 text-lg">
