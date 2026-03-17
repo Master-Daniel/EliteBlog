9041a6e91292b2965d0981bd649ea27153d67370
Master Daniel <engrdanywiss@gmail.com>
2026-03-17 13:29:05 +0100

    refactor SignInModal component to temporarily comment out Twitter sign-in button: updated code for better clarity and organization while preparing for future OAuth integration.

--- src/components/SignInModal.tsx
+++ src/components/SignInModal.tsx
@@ -149,28 +149,22 @@ const SignInModal: React.FC<SignInModalProps> = ({ isOpen, onClose }) => {
                         </svg>
                         Sign in with GitHub
                     </button>
+                    {/* Not configured yet – uncomment when X (Twitter) OAuth is set up
                     <button className="w-full cursor-pointer py-3 flex items-center justify-center gap-3 bg-sky-500 hover:bg-sky-600 text-white font-medium rounded-lg transition focus:ring-2 focus:ring-sky-400">
-                        <svg
-                            role="img"
-                            viewBox="0 0 24 24"
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="w-5 h-5"
-                        >
+                        <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className="w-5 h-5">
                             <title>X</title>
-                            <path
-                                fill="currentColor"
-                                d="M18.901 1.153h3.68l-8.04 9.19L24 22.846h-7.406l-5.8-7.584-6.638 7.584H.474l8.6-9.83L0 1.154h7.594l5.243 6.932ZM17.61 20.644h2.039L6.486 3.24H4.298Z"
-                            />
+                            <path fill="currentColor" d="M18.901 1.153h3.68l-8.04 9.19L24 22.846h-7.406l-5.8-7.584-6.638 7.584H.474l8.6-9.83L0 1.154h7.594l5.243 6.932ZM17.61 20.644h2.039L6.486 3.24H4.298Z" />
                         </svg>
                         Sign in with Twitter
                     </button>
                     <button className="w-full cursor-pointer py-3 flex items-center justify-center gap-3 bg-blue-800 hover:bg-blue-900 text-white font-medium rounded-lg transition focus:ring-2 focus:ring-blue-600">
                         <svg stroke="currentColor" role="img" fill="currentColor" className="w-5 h-5" strokeWidth="0" viewBox="0 0 448 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
                             <title>LinkedIn</title>
-                            <path d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"></path>
+                            <path d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z" />
                         </svg>
                         Sign in with LinkedIn
                     </button>
+                    */}
                 </div>
 
                 <button
