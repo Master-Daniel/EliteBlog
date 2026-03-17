ea323ac0da1707fcf4162208512519dd48a4ed21
Master Daniel <engrdanywiss@gmail.com>
2026-03-17 17:28:57 +0100

    refactor TextEditor component to streamline TinyMCE plugin configuration: removed unnecessary plugins from the menubar and adjusted mobile settings for improved clarity and performance.

--- src/components/dashboard/TextEditor.tsx
+++ src/components/dashboard/TextEditor.tsx
@@ -76,10 +76,10 @@ const TextEditor = forwardRef(({ onChange, initialContent }: TextEditorProps, re
                 icons_url: "/icons/default/icons.min.js",
                 height: 500,
                 plugins:
-                    'importword exportword exportpdf ai preview casechange importcss searchreplace autolink autosave save directionality advcode visualblocks visualchars fullscreen image link math media mediaembed codesample table charmap pagebreak nonbreaking anchor tableofcontents insertdatetime advlist lists checklist wordcount tinymcespellchecker a11ychecker editimage help formatpainter permanentpen pageembed charmap quickbars linkchecker advtable footnotes mergetags autocorrect typography advtemplate markdown',
+                    'importword exportword exportpdf ai preview casechange importcss searchreplace autolink autosave save directionality advcode visualblocks visualchars fullscreen image link math media mediaembed codesample table charmap pagebreak nonbreaking anchor tableofcontents insertdatetime advlist lists checklist wordcount tinymcespellchecker a11ychecker editimage formatpainter permanentpen pageembed charmap quickbars linkchecker advtable footnotes mergetags autocorrect typography advtemplate markdown',
                 mobile: {
                     plugins:
-                        'ai preview casechange importcss searchreplace autolink autosave save directionality advcode visualblocks visualchars fullscreen image link math media mediaembed codesample table charmap pagebreak nonbreaking anchor tableofcontents insertdatetime advlist lists checklist wordcount tinymcespellchecker a11ychecker help formatpainter pageembed charmap mentions quickbars linkchecker advtable footnotes mergetags autocorrect typography advtemplate',
+                        'ai preview casechange importcss searchreplace autolink autosave save directionality advcode visualblocks visualchars fullscreen image link math media mediaembed codesample table charmap pagebreak nonbreaking anchor tableofcontents insertdatetime advlist lists checklist wordcount tinymcespellchecker a11ychecker formatpainter pageembed charmap mentions quickbars linkchecker advtable footnotes mergetags autocorrect typography advtemplate',
                 },
                 images_upload_handler: handleImageUpload,
                 automatic_uploads: true,
@@ -216,7 +216,7 @@ const TextEditor = forwardRef(({ onChange, initialContent }: TextEditorProps, re
                 autocorrect_capitalize: true,
                 toolbar_mode: 'sliding',
                 contextmenu: 'link image editimage table configurepermanentpen',
-                menubar: 'file edit view insert format tools table tc help',
+                menubar: 'file edit view insert format tools table tc',
                 toolbar:
                     "undo redo | importword exportword exportpdf | revisionhistory | aidialog aishortcuts | blocks fontsizeinput | bold italic | align numlist bullist | link image | table math media pageembed | lineheight  outdent indent | strikethrough forecolor backcolor formatpainter removeformat | charmap checklist | code fullscreen preview | save print | pagebreak anchor codesample footnotes mergetags | addtemplate inserttemplate | ltr rtl casechange | spellcheckdialog a11ycheck", // addcomment showcomments
                 content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }',
